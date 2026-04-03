fda768ec2505ed43da472d95c6fdfef08af952e7
me <tkutlesa@gmail.com>
2026-04-03 08:45:15 -0400

    fix(admin): prospects are using annotations instead of N+1

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models import Exists, OuterRef
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 from django.utils.html import format_html
 
 from .models import ColdCallRecord, Prospect
@@ -27,10 +30,24 @@ class ProspectAdmin(admin.ModelAdmin):
         "industry",
         "phone_number",
         "display_website_url",
-        "has_been_called",
+        "called_status",
     ]
     search_fields = ["business_name"]
     inlines = [ColdCallRecordInline]
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        queryset = super().get_queryset(request)
+        return queryset.annotate(
+            has_been_called_db=Exists(
+                ColdCallRecord.objects.filter(prospect_id=OuterRef("pk"))
+            )
+        )
+
+    @admin.display(
+        boolean=True, description="Has been called", ordering="has_been_called_db"
+    )
+    def called_status(self, obj):
+        return obj.has_been_called_db
 
     def display_website_url(self, obj: Prospect):
         return format_html(
