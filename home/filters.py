4aeb5e928eb6981b058459e56f45ca61aa6723e1
me <tkutlesa@gmail.com>
2026-04-08 03:20:17 -0400

    feat(filters): update ProspectsFilter to use TypedChoiceFilter for boolean fields

--- home/filters.py
+++ home/filters.py
@@ -4,15 +4,23 @@ from . import models
 from .forms import ProspectsFilterSetForm
 
 
+def _coerce_bool(value: str) -> bool:
+    return value.lower() in {"1", "true", "t", "yes", "y", "on"}
+
+
 class ProspectsFilter(django_filters.FilterSet):
-    industry = django_filters.AllValuesFilter(
-        field_name="industry", label="Industry", empty_label="All"
+    industry = django_filters.AllValuesFilter(field_name="industry", label="Industry")
+    called = django_filters.TypedChoiceFilter(
+        field_name="called",
+        label="Called",
+        choices=(("", "All"), ("true", "Yes"), ("false", "No")),
+        coerce=_coerce_bool,
     )
-    called = django_filters.BooleanFilter(
-        field_name="called", label="Called", empty_label="All"
-    )
-    conversation = django_filters.BooleanFilter(
-        field_name="conversation", label="Conversation", empty_label="All"
+    conversation = django_filters.TypedChoiceFilter(
+        field_name="conversation",
+        label="Conversation",
+        choices=(("", "All"), ("true", "Yes"), ("false", "No")),
+        coerce=_coerce_bool,
     )
 
     class Meta:
